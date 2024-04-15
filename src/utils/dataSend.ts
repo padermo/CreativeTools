980c2d6dbbced103c9088d29052da317e2599aaa
padermo <fabioaaeh96@gmail.com>
2024-04-15 15:07:07 -0500

    adding subcategory ia

--- src/utils/dataSend.ts
+++ src/utils/dataSend.ts
@@ -2,7 +2,7 @@ export const category = ['documents','resources','modules'];
 
 export const subcategory = {
   documents: ['programming languages','frameworks','libraries','APIs','databases','operating systems','network protocols','others'],
-  resources: ['icons','colors','images','animations','alerts','compressors','gradients','emulators','carousels','components','patterns','editors','fonts','generators','videos','mockups','templates','vectors','sounds','others'],
+  resources: ['icons','colors','images','animations','alerts','compressors','gradients','emulators','carousels','components','patterns','editors','fonts','generators','videos','mockups','templates','vectors','sounds','ia','others'],
   modules: ['npm','yarn','pnpm','pipy','others']
 }
 
