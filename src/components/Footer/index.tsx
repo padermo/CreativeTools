99abf1b667c1d387e23ce36814c8a904b2bbeeb0
padermo <fabioaeh96@gmail.com>
2024-09-08 18:14:57 -0500

    modifying bg in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,13 +2,13 @@
 import { useTranslations } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
 
-export default function Footer(){
+export default function Footer() {
   const t = useTranslations('Footer');
   const searchParams = useSearchParams();
   const viewPolicyCookies = searchParams.get('view');
 
-  return(
-    <footer className={`w-full h-14 bg-[#523061] text-white font-light text-sm text-center flex items-center justify-center px-4 md:px-6 lg:px-8 ${viewPolicyCookies === 'cookies' && 'hidden'}`}>
+  return (
+    <footer className={`w-full h-14 bg-[#0b0b0b] text-white font-light text-sm text-center flex items-center justify-center px-4 md:px-6 lg:px-8 ${viewPolicyCookies === 'cookies' && 'hidden'}`}>
       <p>&copy; {t('title')}</p>
     </footer>
   )
