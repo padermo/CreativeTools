5a8c7287d13e75c26e35775359c43d0ee1a2ced8
padermo <fabioaeh96@gmail.com>
2024-09-08 15:16:23 -0500

    modifying Landig components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,15 @@
-import Header from '@/components/landing/Header'
-import Search from '@/components/landing/Search'
-import Account from '@/components/landing/Account'
+import Header from '@/components/Landing/Header'
+import Search from '@/components/Landing/Search'
+import Account from '@/components/Landing/Account'
 import FloatContact from '@/components/contact/FloatContact'
 import PageLayout from '@/components/ParentComponents/PageLayout'
 
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <PageLayout>
-      <Header/>
-      <Search/>
-      {/*<Account/>*/}
+      <Header />
+      <Search />
+      <Account/>
       {/*<FloatContact/>*/}
     </PageLayout>
   )
--- src/components/Landing/Account.tsx
+++ src/components/Landing/Account.tsx
@@ -1,22 +1,22 @@
 'use client'
 import { useTranslations } from 'next-intl'
 import { useRouter } from '@/navigation';
-import ButtonReusable from '../reusable/ButtonReusable';
+import ButtonReusable from '../Reusable/Button';
 import Join from '../svg/Join';
 
-export default function Account(){
+export default function Account() {
   const t = useTranslations('Content.Account');
   const router = useRouter();
 
-  return(
+  return (
     <article className='w-full min-h-dvh max-h-full flex flex-col px-4 lg:justify-normal lg:flex-row md:px-6 lg:px-8'>
       <picture className='w-full h-dvh hidden items-end lg:flex'>
-        <Join/>
+        <Join />
       </picture>
-      <div className='w-full h-dvh flex flex-col justify-center items-center text-center gap-6 text-wrap text-[#222] text-lg font-light tracking-wide dark:text-white'>
+      <div className='w-full h-dvh flex flex-col justify-center items-center text-center gap-6 text-wrap text-lg font-light tracking-wide'>
         <h3 className='w-full md:w-1/2 lg:w-1/2'>{t('title')}</h3>
         <div>
-          <ButtonReusable type='primary' loading={false} onClick={()=>router.push('/auth/register')} >{t('cta')}</ButtonReusable>
+          <ButtonReusable text={t('cta')} htmlType='button' type='primary' loading={false} onClick={() => router.push('/auth/register')} />
         </div>
       </div>
     </article>
