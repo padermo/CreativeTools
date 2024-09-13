cee19daa12ed7c247d8bbefd9fdfa8618e5602d9
padermo <fabioaeh96@gmail.com>
2024-09-13 16:38:52 -0500

    fixed skeleton

--- src/components/Items/Card/Cards.tsx
+++ src/components/Items/Card/Cards.tsx
@@ -8,9 +8,9 @@ export default function Cards() {
   const { items, loadingItems } = useItems();
 
   if (loadingItems) return <CardSkeleton />;
+  if (!items.length) return <NotData />;
   return (
     <div className="w-full h-full flex-1 grid justify-center md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-3 xl:grid-cols-5 xl:grid-rows-2 2xl:grid-cols-4 2xl:grid-rows-3 gap-5">
-      {!items.length && <NotData />}
       {items &&
         items.map((item) => (
           <Card
--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -1,67 +1,67 @@
-import { Skeleton } from 'antd';
+import { Skeleton } from "antd";
 
 export default function CardSkeleton() {
   return (
-    <div className='w-full flex flex-wrap justify-center gap-5 flex-1 md:justify-between lg:items-start lg:justify-between 2xl:items-center'>
+    <div className="w-full flex flex-wrap justify-center gap-5 flex-1 md:justify-between lg:items-start lg:justify-between 2xl:items-center">
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
       <Skeleton.Avatar
         active
         size={240}
-        shape='square'
-        style={{ borderRadius: '6px' }}
+        shape="square"
+        style={{ borderRadius: "6px", backgroundColor: "#1b1b1b" }}
       />
     </div>
   );
