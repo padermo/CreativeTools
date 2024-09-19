022b3f0372c6108442ba9ed92027f66519ad1864
padermo <fabioaeh96@gmail.com>
2024-09-19 18:07:59 -0500

    modifying action.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,8 +4,6 @@ import { cookies } from "next/headers";
 export async function createCookie(token: string) {
   cookies().set("creative-auth-token", token, {
     expires: 60 * 60 * 24 * 15,
-    httpOnly: true,
-    secure: true,
   });
 }
 
