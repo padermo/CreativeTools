c05ffd3be584e2e3df8a807289b592468bf9efd6
padermo <fabioaeh96@gmail.com>
2024-09-19 18:20:00 -0500

    adding expires in createCookie

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,9 +2,8 @@
 import { cookies } from "next/headers";
 
 export async function createCookie(token: string) {
-  cookies().set("creative-auth-token", token, {
-    expires: 60 * 60 * 24 * 15,
-  });
+  const expires = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
+  cookies().set("creative-auth-token", token, { expires });
 }
 
 export async function removeCookie() {
